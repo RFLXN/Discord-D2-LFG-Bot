84d9dbafbdeb252c45dce0b1a22d2e423148cc8e
jhchoi123 <solid2113@naver.com>
2022-12-28 22:46:10 +0900

    add pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -3,7 +3,8 @@ module.exports = {
         {
             name: "DiscordD2LfgBot",
             script: "./dist/main.js",
-            cwd: "/home/ubuntu/Discord-D2-LFG-Bot",
+            node_args: "--experimental-specifier-resolution=node",
+            cwd: "/home/ubuntu/Discord-D2-LFG-Bot/",
             log_date_format: "YYYY-MM-DD HH:mm Z",
             error_file: "/home/ubuntu/Discord-D2-LFG-Bot/logs/error.log",
             out_file: "/home/ubuntu/Discord-D2-LFG-Bot/logs/out.log"
